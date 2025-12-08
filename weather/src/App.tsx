60d564fbda7555ea199b6e22050f45af4e6d3200
KimHyeonSu <hirobin2001@gmail.com>
2025-12-08 13:59:21 +0900

    Feat: 폴더 구조 생성

---
+++ weather/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './pages/Header'
+
+function App() {
+
+  return (
+    <>
+      {/* 헤더 */}
+        <Header />
+      {/* 오늘 날씨/내일 날씨/지수 정보 */}
+
+      {/* 주간 날씨 정보 */}
+    </>
+  )
+}
+
+export default App
---
+++ weather/src/pages/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <div>날씨 정보</div>
+      <div>위치 정보</div>
+    </div>
+  )
+}
+
+export default Header;
